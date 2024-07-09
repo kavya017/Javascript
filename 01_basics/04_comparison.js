f6117aa07c8139fa00f42cd4f28d9588e4690ff9
kavya <107758200+kavya017@users.noreply.github.com>
2024-07-09 08:54:28 +0000

    coparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+/*console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);*/
+
+//console.log("2">1);
+//console.log("02">1); // datatype should be same
+
+//AVOID
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+
+// === strict check
+
+console.log("2"===2);
