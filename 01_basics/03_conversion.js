f5896ca851dbb8e8dd4ab63dfdf40a4602ab8af9
kavya <107758200+kavya017@users.noreply.github.com>
2024-07-09 08:05:23 +0000

    conversion and operation

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -11,11 +11,45 @@
 //"33abc"==NaN
 //true==1;false==0
 
-let isLoggedIn=1
+/*let isLoggedIn=1
 
 let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);*/
 
 //1==>true; 0==>false
 //" "==false
 //"hitesh"==>true
+
+//*********************OPERATION ****************************
+
+let value=3;
+let negValue=-value;
+//console.log(negValue);
+//console.log(2+2);
+//console.log(n2*2);
+
+let str1="hello"
+let str2="kavya"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1+"2");
+console.log("1"+2+2);// if string is first conversion will be in string
+console.log(1+2+"2");// if number is first , operation will be first then conversion in sgtring
+
+
+console.log(3+4*5%3);// not a good practice
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
