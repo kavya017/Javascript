73581eaf4553092cfb0a19b0c284bf2340cdede9
kavya <107758200+kavya017@users.noreply.github.com>
2024-07-20 07:23:22 +0000

    Number and Maths

---
+++ 01_basics/06_numberNmaths.js
@@ -0,0 +1,19 @@
+const score=400
+//console.log(score);
+
+const balance= new Number(100)
+//console.log(balance.toString().length);
+
+const hundreads=1000000
+//console.log(hundreads.toLocaleString('en-IN'));
+
+//-------------------MATHS-----------------
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+
+const min=10
+const max=20
+//imp
+console.log(Math.floor(Math.random()*( max-min+1))+min);
